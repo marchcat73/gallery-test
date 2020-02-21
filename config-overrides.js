311d7f4436597d331c20285a1877d9a111b17e3c
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2020-02-21 13:26:41 +0400

    first commit

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+module.exports = config => {
+  require('react-app-rewire-postcss')(config, {
+    plugins: loader => [
+      require('postcss-preset-env')({
+        stage: 1
+      }),
+      require('postcss-safe-parser'),
+      require('postcss-import'),
+      require('postcss-nested'),
+      require('autoprefixer'),
+      require('postcss-extend'),
+      require('postcss-all-link-colors'),
+      require('postcss-css-variables'),
+      require('postcss-minimize')
+    ]
+  });
+
+  return config;
+};
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles/main.scss';
+
+function App() {
+  return <div className="App">1</div>;
+}
+
+export default App;
